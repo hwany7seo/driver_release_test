8298a18a2a0d147bb41d94173a6ab5504d53fecc
vboxuser <hwanyseo@cubrid.com>
2025-02-12 13:36:36 +0900

    remove lib

--- win/pdo/lib/devel-pack/php-devel-pack-5.6.31-Win32-VC11-x64/php-5.6.31-devel-VC11-x64/include/ext/date/lib/timelib_config.h
+++
@@ -1,5 +0,0 @@
-#include "config.w32.h"
-#include <php_stdint.h>
-#include "win32/php_inttypes.h"
-#define TIMELIB_OMIT_STDINT 1
-#define TIMELIB_FORCE_LONG32 1
--- win/pdo/lib/devel-pack/php-devel-pack-5.6.31-nts-Win32-VC11-x64/php-5.6.31-devel-VC11-x64/script/config.phpize.js
+++
@@ -1,8 +0,0 @@
-var PHP_ZTS ="no"
-var PHP_DLL_LIB ="php5.lib"
-var PHP_DLL ="php5.dll"
-var PHP_ANALYZER ="no"
-var PHP_PGO ="no"
-var PHP_PGI ="no"
-var PHP_ALL_SHARED ="no"
-
